a45612284f5dc84da8e3a507c38e91431a7c0ecd
Iliya Tazalov <134263398+tazalov@users.noreply.github.com>
2023-07-20 21:49:16 +0300

    Create Slider, Feedback component, refactor styles for SectionTitle

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Header } from "./layout/Header/Header";
 import { Main } from "./layout/Sections/Main/Main";
 import { Skills } from "./layout/Sections/Skills/Skills";
 import { Projects } from "./layout/Sections/Projects/Projects";
+import { Feedback } from "./layout/Sections/Feedback/Feedback";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Main />
       <Skills />
       <Projects />
+      <Feedback />
     </div>
   );
 }
--- src/components/SectionTitle/SectionTitle.tsx
+++ src/components/SectionTitle/SectionTitle.tsx
@@ -1,6 +1,16 @@
-import React from "react";
 import styled from "styled-components";
 
 export const SectionTitle = styled.h2`
   color: red;
+  position: relative;
+  display: inline-block;
+  &:after {
+    content: "";
+    width: 50vw;
+    height: 2px;
+    background-color: red;
+    position: absolute;
+    left: 105%;
+    bottom: 30%;
+  }
 `;
